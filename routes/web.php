8ff7680e5512fd4883aa5f1a20bba3b893b851da
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-06-03 18:28:50 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+use App\Http\Controllers\Admin\Postcontroller;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::middleware(['auth', 'verified'])
+->name('admin.')
+->prefix('admin')
+->group(function () {
+    
+    Route::get('/', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+
+    //REGISTRIAMO LE ALTRE ROTTE PROTETTE
+    Route::resource('posts', Postcontroller::class);
+
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
